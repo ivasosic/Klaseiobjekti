1a88ec5754e4368386fca3f9e299b6f3684ecf16
a <145933200+ivasosic@users.noreply.github.com>
2023-11-29 09:34:39 +0100

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace klaseIObjekti
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Zrakoplov x380=new Zrakoplov("Airbus", 2000, 6000);
+
+            Console.Write(x380.ToString());
+
+            Zrakoplov C162 = new Zrakoplov("Cessna Skycatcher", 75, 870);
+
+            Console.Write(C162.ToString());
+
+            Zrakoplov PC21 = new Zrakoplov("Pilatus", 1200, 1333);
+
+            Console.Write(PC21.ToString());
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Zrakoplov.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace klaseIObjekti
+{
+    internal class Zrakoplov
+    {
+        string naziv;
+        int snagamotora, dosegleta;
+
+        public string Naziv1 { get => naziv; set => naziv = value; }
+        public int SnagaMotora1 { get => snagamotora; set => snagamotora = value; }
+        public int DosegLeta1 { get => dosegleta; set => dosegleta = value; }
+
+        public Zrakoplov(String Naziv, int SnagaMotora, int DosegLeta)
+        {
+            this.naziv = naziv;
+            this.snagamotora=snagamotora;
+            this.dosegleta=dosegleta;
+
+        }
+        public Zrakoplov(){ }
+
+
+        public override string ToString()
+        {
+            string ispis = "Naziv: " + this.Naziv1 + "\n"
+                + "Snaga motora: " + this.SnagaMotora1 + " kW\n"
+                + "Doseg leta: " + this.DosegLeta1 + " km\n";
+            return ispis;
+        }
+    }
+}
